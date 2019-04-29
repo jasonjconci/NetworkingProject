7f903d43783ff14abbd850a0249467a9ae9ae838
Daniel Abrahms <dabrahms@zagmail.gonzaga.edu>
2019-04-28 20:08:17 -0700

    Changed Network data

---
+++ networking-project/src/Utilities.js
@@ -0,0 +1,5 @@
+class Utilities {
+    testing() {
+        return "hello";
+    }
+}
--- networking-project/src/index.js
+++ networking-project/src/index.js
@@ -37,15 +37,27 @@ class Main extends React.Component {
             { id: 2, label: "B" },
             { id: 3, label: "C" },
             { id: 4, label: "D" },
-            { id: 5, label: "E" }
+            { id: 5, label: "E" },
+            { id: 6, label: "F" },
+            { id: 7, label: "G" },
+            { id: 8, label: "H" },
+            { id: 9, label: "I" }
         ]);
 
         // create an array with edges
         var edges = new vis.DataSet([
-            { from: 1, to: 3 },
-            { from: 1, to: 2 },
-            { from: 2, to: 4 },
-            { from: 2, to: 5 }
+            { from: 1, to: 3, label: "5" },
+            { from: 1, to: 4, label: "2" },
+            { from: 2, to: 3, label: "6" },
+            { from: 2, to: 5, label: "2" },
+            { from: 2, to: 6, label: "1" },
+            { from: 3, to: 5, label: "1" },
+            { from: 4, to: 5, label: "4" },
+            { from: 4, to: 7, label: "7" },
+            { from: 5, to: 7, label: "3" },
+            { from: 5, to: 8, label: "3" },
+            { from: 6, to: 8, label: "4" },
+            { from: 7, to: 9, label: "2" }
         ]);
 
         // create a network
